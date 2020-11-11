bb58acaca09e98083c281dc9142c69092426f366
Hendra Ng <hendrang@outlook.com>
2020-11-10 23:21:43 -0700

    Update site

--- src/pages/index.js
+++ src/pages/index.js
@@ -11,11 +11,11 @@ const Index = (props) => {
       </div>
       <div className="container">
         <div className="content is-family-primary">
-          <p>Let me introduce myself as a software developer. Currently, I am working full-time at Charles Schwab as lead developer. In the past, I worked as Software Developer Associate Manager at Accenture, Senior Software Engineer (Tech Lead) at Charles Schwab and Senior Application Programmer at Progressive Insurance.</p>
-          <p>My career in technology can perhaps be traced back to when I was a teenager. With my friend’s help, I created my first my personal website using Yahoo website builder. Of course I wanted to have a cool site with fireworks and counters. Since then, I have been “addicted” to the web and other technologies. I am always amazed by how far technology has gone in order to solve problems effectively and efficiently. Making life and/or business easier with technology is great!</p>
-          <p>With my personal site, I would like to market my experience (<Link href="/resume">see resume</Link>) and hope to connect with other fellow developers and/or entrepreneurs to solve real world problems. With my experience and willingness to learn, I hope I can work with you on any type of projects.</p>
-          <p>From the quote above, I’m sure you know by now that I’m a huge fan of Michael “Air” Jordan. If you happen to live in Colorado and have some pick-up games, you can count on this average basketball player.</p>
-          <p><br/>Hendra Ng</p>
+          <p>Let me start by introducing myself. I am a new dad/husband and software developer (in that order) at Charles Schwab. </p>
+          <p>My interest in technology can be traced back to my high school time. With my friend’s help, I launched my first personal website using Yahoo website builder with the help of 52k dialup modem. It's a cool site with fireworks, counters, and don't forget the background music! Web technologies definitely helped me sharing my information to my friends and the world (dial-up modem speed) fast. Technology does make our lives easier!</p>
+          <p>With my current personal site, my hope is to connect with other problem solvers. Feel free to check out my (<Link href="/resume">resume</Link>). I also plan to share more tips/tricks based on my challenges learning new things through my <Link href="/blog">blog</Link>. Perhaps, you can gain something from it. You can think of it as how-to documentation.</p>
+          <p>Technology aside, I'm sure you can guess my hobby from the above quote. I would love to get some pick-up games (or golf) whenever possible.</p>
+          <p><br />Hendra Ng</p>
         </div>
       </div>
     </section>
